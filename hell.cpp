9080ae290695f2646ac793b0d40a615bf1890cc9
ronghaiyang <ronghaiyang@gmail.com>
2013-09-26 21:10:16 +0800

    ceshi3 jia   while false  int ii

--- hell.cpp
+++ hell.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
 using namespace std;
 int main(){
+    
+    while(false){
+        
+    }
+    
+    int j;
+    cin>>j;
+    
   std::cout<<"hello world ,my github!"<<endl;
     for(int i =0 ;i < 3; i ++)
         ;
+    int ii = ;
   return 0;
 }
